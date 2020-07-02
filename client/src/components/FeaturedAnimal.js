dc3a159494e2e729cbf5eb2929e652ede7eaab8e
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-07-01 20:36:17 -0500

    changed filtering for api

--- client/src/components/FeaturedAnimal.js
+++ client/src/components/FeaturedAnimal.js
@@ -3,11 +3,11 @@ import React from "react";
 function FeaturedAnimal(props) {
   return (
     <div>
-      <img src={props.photo}></img>
+      <img src={props.photo} alt={props.name}></img>
       <p>
-        Hi my name is {props.name}, I am a{props.species}, specifically a{" "}
-        {props.gender}
-        {props.breed}. I am currently {props.status}
+        Hi my name is {props.name}! I am a {""}
+        {props.species}, specifically a {props.gender} {props.breed}. I am
+        currently {props.status}
       </p>
     </div>
   );

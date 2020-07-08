fecc908981c9757e7f62c4d9ecbccb31af3de304
Kilea Castillo <59230922+norwayflames@users.noreply.github.com>
2020-07-08 07:18:37 -0600

    working with passport as its own folder, working with stratigies.

---
+++ passport/SigninStrategy.js
@@ -0,0 +1,13 @@
+const Strategy = require('passport-local').Strategy;
+
+const LoginStrategy = new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!user.verifyPassword(password)) { return done(null, false); }
+        return done(null, user);
+    });
+}
+);
+
+module.exports = LoginStrategy;

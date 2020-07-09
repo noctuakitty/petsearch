5e4d00bfca55dd127ef0b7bfcb2bdb684ea97394
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-07-08 20:44:20 -0500

    added zip search form to eventsPage

--- client/src/components/EventsPage.js
+++ client/src/components/EventsPage.js
@@ -2,14 +2,26 @@ import React from "react";
 
 function EventsPage() {
   return (
-  <div className="container">
-    <div className="jumbotron">
-      <h1>Events Near You</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorum quae a quidem at dolor exercitationem veniam. Quidem rem veniam fugit eligendi sunt illo aspernatur deserunt ipsam, sint cupiditate nihil.
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Events Near You</h1>
+        <p>
+          Find an adoption event near you and bring home your new best friend!
+          Just input your zip code to use the event finder then go meet Fluffy!{" "}
         </p>
+      </div>
+      <form className="form-inline my-2 my-lg-0">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Enter Zip Code"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Find Events
+        </button>
+      </form>
     </div>
-  </div>
   );
 }
 

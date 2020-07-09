7f86d0eef4de7d38864a0d748f8d2223530944fe
Anibal Guerra <guerra15anibal@hotmail.com>
2020-07-08 21:14:31 -0600

    adding more login

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,17 @@
 const axios = require("axios")
+
+export default{
+    getLogin: function (userData) {
+        console.log("user data", userData)
+        return axios.post("/login", userData)
+    },
+    signUp: function (userData) {
+        return axios.post("/register", userData)
+    },
+    getUser: function () {
+        return axios.get("/user")
+    },
+    logOut: function () {
+        return axios.get("/logout")
+    }
+}
